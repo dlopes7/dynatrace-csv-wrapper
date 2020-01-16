99ca0eb321f3aeeeb135b2afd309c1361460945c
David Lopes <david.lopes@dynatrace.com>
2020-01-16 08:49:47 -0300

    Change port to 80

--- classicwrapper/app.py
+++ classicwrapper/app.py
@@ -191,7 +191,7 @@ def timeseries(identifier):
 
 def main():
     log_setup()
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=80, debug=True)
 
 
 if __name__ == "__main__":
